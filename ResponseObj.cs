60f166fe52c9240e5e4d3f8ffa0a052051cc34bd
APIVerve Team <hello@apiverve.com>
2024-05-14 15:46:41 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,51 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class holidays
+{
+    [JsonProperty("date")]
+    public string date { get; set; }
+
+    [JsonProperty("start")]
+    public DateTime start { get; set; }
+
+    [JsonProperty("end")]
+    public DateTime end { get; set; }
+
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("type")]
+    public string type { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("country")]
+    public string country { get; set; }
+
+    [JsonProperty("year")]
+    public string year { get; set; }
+
+    [JsonProperty("holidays")]
+    public holidays[] holidays { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
