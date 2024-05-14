70ba9e43546d352da6880a4f6ef193611dd88ec1
APIVerve Team <hello@apiverve.com>
2024-05-13 22:44:31 -0500

    Initial Commit

---
+++ QueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class QueryOptions {
+public string country { get; set; }
+public int year { get; set; }
+}
+
+}
